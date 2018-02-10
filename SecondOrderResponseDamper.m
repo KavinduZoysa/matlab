2881d6633aa2cbee649fa45c7b1662825494d944
kavindu <kavindu@wso2.com>
2018-02-10 10:54:26 +0530

    Add root locus functions

--- SecondOrderResponseDamper.m
+++ SecondOrderResponseDamper.m
@@ -28,12 +28,16 @@ if delta > 0
 
     yH = p1*exp(alpha1*t) + p2*exp(alpha2*t);
     yE = eta*A*(q3 + q1*exp(alpha1*t) + q2*exp(alpha2*t));
+    h1 = tf([1, 2*sigma, rho], -1);
+    subplot(311); rlocus(h1); grid on;
 elseif delta == 0
     alpha = -sigma;
     p5 = (k1*alpha + k2)/alpha; p6 = -k2/alpha; p7 = 1/alpha; p8 = -p7;
     
     yH = exp(alpha*t)*(p6 + p5) + p5*alpha*t.*exp(alpha*t);
     yE = eta*A*(-p7*t.*exp(alpha*t) + p8*(exp(alpha*t)-1)/alpha);
+    h2 = tf([1, 2*sigma, rho], -1);
+    subplot(312); rlocus(h2); grid on;
 else
     omega = sqrt(-delta);
     phiH = atan2(k1, (k2 - k1*sigma)/omega);
@@ -43,15 +47,20 @@ else
     yH = K*exp(-sigma*t).*sin(omega*t+phiH);
     yE1 = exp(-sigma*t).*sin(omega*t+phiE);
     yE = yH + eta*A*(omega/(omega^2 + sigma^2) - yE1)/omega;
+    h3 = tf([1, 2*sigma, rho], -1);
+    subplot(313); rlocus(h3); grid on;
 end    
 
+% h = tf([1, 2*sigma, rho], -1);
 yT = yH + yE;
 
-subplot(311); plot(t,yH); axis([0 duration -2.8 15]);
-ylabel('Homogeneous Response [V]'); grid on;
+% subplot(311); plot(t,yH); axis([0 duration -2.8 15]);
+% ylabel('Homogeneous Response [V]'); grid on;
+% 
+% subplot(312); plot(t,yE); axis([0 duration -2.8 15]);
+% ylabel('Exogeneous Response [V]'); grid on;
+% 
+% subplot(313); plot(t,yT); axis([0 duration -2.8 15]);
+% ylabel('Total Response [V]'); grid on;
 
-subplot(312); plot(t,yE); axis([0 duration -2.8 15]);
-ylabel('Exogeneous Response [V]'); grid on;
-
-subplot(313); plot(t,yT); axis([0 duration -2.8 15]);
-ylabel('Total Response [V]'); grid on;
+ 
